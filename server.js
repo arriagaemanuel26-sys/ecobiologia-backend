d77277cf24694e2caed11b69cbbc9daa37162725
arriagaemanuel26-sys <arriagaemanuel26@gmail.com>
2026-05-15 00:54:09 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type']
+}));
+
+app.options('*', cors());
 app.use(express.json());
 
 app.post('/api/dato', async (req, res) => {
